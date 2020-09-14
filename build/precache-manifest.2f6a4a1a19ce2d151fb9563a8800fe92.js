d58f5a2a782ee3c3e2a10bd6b132014f78097683
nancyjoshlin <67499977+nancyjoshlin@users.noreply.github.com>
2020-09-14 12:57:56 +0530

    first commit

---
+++ build/precache-manifest.2f6a4a1a19ce2d151fb9563a8800fe92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acda143389b426b272c3c2c2239f25e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cb54aed2cc7f3a83b3b",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "5106fb1c7d542b225aa5",
+    "url": "/static/css/main.cfbdf84b.chunk.css"
+  },
+  {
+    "revision": "2cb54aed2cc7f3a83b3b",
+    "url": "/static/js/2.4734ccf1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4734ccf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5106fb1c7d542b225aa5",
+    "url": "/static/js/main.13cde538.chunk.js"
+  },
+  {
+    "revision": "29ee44826ab01071335c",
+    "url": "/static/js/runtime-main.8704d4bd.js"
+  }
+]);
